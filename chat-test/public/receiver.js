9b394fd836df1d2b17e289bdff7c6ba4164d9ac6
JosseVessies1990 <59568003+JosseVessies1990@users.noreply.github.com>
2024-09-30 00:24:15 +0200

    update check

--- chat-test/public/receiver.js
+++ chat-test/public/receiver.js
@@ -38,6 +38,7 @@ individual_stream.prototype.setDestination = function() {
   this.finalstream = this.AUDIOcontext.createMediaStreamSource(this.combinedSTREAM)
   this.destination = this.AUDIOcontext.createMediaStreamDestination()
   this.finalstream.connect(this.destination)
+  this.finalstream.connect(this.AUDIOcontext.destination);
   console.log('destination created : ', this.destination.stream)
 };
 
