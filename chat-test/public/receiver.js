cc0767846cff3e7f3a38273c4eb459cb7ea74d2f
JosseVessies1990 <59568003+JosseVessies1990@users.noreply.github.com>
2024-10-13 17:03:39 +0200

    pushing new receiver

--- chat-test/public/receiver.js
+++ chat-test/public/receiver.js
@@ -65,7 +65,7 @@ individual_stream.prototype.connectStreams = function(){
     // console.log("mute track number is: ", this.muteTRACK)
     // console.log("this POSITION, ", this.Position)
     if(this.Position != 0){
-      console.log('this position, ', this.position)
+      console.log('this position, ', this.Position)
       console.log('mutetracks includes the number  ', i, ' MUTRTRACKS = ', MUTETRACKS)
       if(!MUTETRACKS.includes(i)){
         console.log("THIS SHOULDN't HAPPEN")
