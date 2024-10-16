991b71daabd0655a11197ad7d7f45a857462000c
JosseVessies1990 <59568003+JosseVessies1990@users.noreply.github.com>
2024-10-16 23:55:50 +0200

    phone up do date

--- chat-test/app.js
+++ chat-test/app.js
@@ -346,7 +346,7 @@ function onConnected(socket){
 
     socket.on('kick-user', (username)=>{
         // io.emit("remote-console", "USER BEING KICKED")
-        io.emit("remote-console", dictionary[])
+        io.emit("remote-console", dictionary[peerSocketIDMap[username]][0])
         io.emit("remote-console", users)
         if(users[peerSocketIDMap[username]]){
             let IP = dictionary[peerSocketIDMap[username]][0]
