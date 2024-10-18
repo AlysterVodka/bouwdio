1a08f36bd02d7382b3801c0268c5d86714450381
JosseVessies1990 <59568003+JosseVessies1990@users.noreply.github.com>
2024-10-18 19:38:47 +0200

    new files for webgrab

--- chat-test/public/main.js
+++ chat-test/public/main.js
@@ -203,7 +203,7 @@ const init = () => {
     });
 
     socket.on('mouses', (data)=>{
-      // console.log(data)
+      console.log(data)
       Object.keys(data).forEach((key, index) => {
         MICE[index].style.left = `${data[key][3].x*gridContainer.width}px`
         MICE[index].style.top = `${data[key][3].y*gridContainer.height}px`
